a5df1301821ec910ac47dd727c39bbe39e653017
tobias-tengler <45513122+tobias-tengler@users.noreply.github.com>
2023-11-06 19:59:23 +0100

    Update NameQuery$parameters

--- app/__generated__/NameQuery$parameters.ts
+++ app/__generated__/NameQuery$parameters.ts
@@ -1,5 +1,5 @@
 /**
- * @generated SignedSource<<d694936b71efce02b4ebcdbbec058dca>>
+ * @generated SignedSource<<4962af806357519378ee80e7bee8c905>>
  * @relayHash 813afd3121599143f2d5bd004bd50fb6
  * @lightSyntaxTransform
  * @nogrep
@@ -11,15 +11,18 @@
 
 // @relayRequestID 813afd3121599143f2d5bd004bd50fb6
 
-import { PreloadableConcreteRequest } from 'react-relay';
+import { PreloadableConcreteRequest } from 'relay-runtime';
 import { NameQuery } from './NameQuery.graphql';
 
-const node: PreloadableConcreteRequest<NameQuery> = { kind: "PreloadableConcreteRequest", params: {
-  "id": "813afd3121599143f2d5bd004bd50fb6",
-  "metadata": {},
-  "name": "NameQuery",
-  "operationKind": "query",
-  "text": null
-} };
+const node: PreloadableConcreteRequest<NameQuery> = {
+  "kind": "PreloadableConcreteRequest",
+  "params": {
+    "id": "813afd3121599143f2d5bd004bd50fb6",
+    "metadata": {},
+    "name": "NameQuery",
+    "operationKind": "query",
+    "text": null
+  }
+};
 
 export default node;
